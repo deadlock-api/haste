b21d02e0e0a7430ab407d26217688dd6324ab758
Manuel Raimann <raimannma@outlook.de>
2025-06-03 16:20:54 +0200

    Remove comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,2 @@
-// TODO(bluaki): nice proper no nonsense public exports.
-// most of the stuff that is exported must not be exported.
-
 pub use haste_broadcast as broadcast;
 pub use haste_core::*;
